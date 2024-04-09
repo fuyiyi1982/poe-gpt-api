8618ea7ac40480db18292032ba7fe61d1639f527
LieToMe <formzs@qq.com>
2024-04-09 20:56:22 +0800

    Update api.py add sync request

--- external/api.py
+++ external/api.py
@@ -4,11 +4,11 @@ import sys
 import logging
 
 import uvicorn
-from fastapi import FastAPI, Request, WebSocket
+from fastapi import FastAPI, Request, WebSocket, Form
 from fastapi.responses import JSONResponse
 import asyncio
 from fastapi_poe.types import ProtocolMessage
-from fastapi_poe.client import get_bot_response
+from fastapi_poe.client import get_bot_response, get_final_response, QueryRequest
 
 file_path = os.path.abspath(sys.argv[0])
 file_dir = os.path.dirname(file_path)
@@ -29,13 +29,17 @@ bot_names = {"Assistant", "ChatGPT-16k", "GPT-4", "GPT-4-128k", "Claude-3-Opus"}
 async def get_responses(api_key, prompt, bot):
     if bot in bot_names:
         message = ProtocolMessage(role="user", content=prompt)
-        buf = ""
-        try:
-            async for partial in get_bot_response(messages=[message], bot_name=bot, api_key=api_key):
-                buf += partial.text
-        except GeneratorExit:
-            return buf
-        return buf
+        additional_params = {"temperature": 0.7, "skip_system_prompt": False, "logit_bias": {}, "stop_sequences": []}
+        query = QueryRequest(
+            query=[message],
+            user_id="",
+            conversation_id="",
+            message_id="",
+            version="1.0",
+            type="query",
+            **additional_params
+        )
+        return await get_final_response(query, bot_name=bot, api_key=api_key)
     else:
         return "Not supported by this Model"
 
@@ -52,7 +56,7 @@ async def stream_get_responses(api_key, prompt, bot):
         yield "Not supported by this Model"
 
 
-def add_token(token):
+def add_token(token: str):
     if token not in client_dict:
         try:
             ret = asyncio.run(get_responses(token, "Please return “OK”", "Assistant"))
@@ -69,12 +73,12 @@ def add_token(token):
 
 
 @app.post("/add_token")
-async def add_token_endpoint(token: str):
+async def add_token_endpoint(token: str = Form(...)):
     return add_token(token)
 
 
 @app.post("/ask")
-async def ask(token: str, bot: str, content: str):
+async def ask(token: str = Form(...), bot: str = Form(...), content: str = Form(...)):
     add_token(token)
     try:
         return await get_responses(token, content, bot)
